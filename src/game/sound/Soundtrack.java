16d344af7cd8ff03acfc8cb5535cf08a58cff1b1
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-18 10:18:53 -0300

    wip: game soundtrack class

---
+++ src/game/sound/Soundtrack.java
@@ -0,0 +1,46 @@
+package game.sound;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Soundtrack {
+    private static final Logger LOGGER = Logger.getLogger(Soundtrack.class.getName());
+    private static final String SOUNDTRACK_PATH = "./src/resources/sound/";
+
+    public Soundtrack() {
+        createDirectory();
+    }
+
+    public static void playSound(String sound) {
+        File file = new File(SOUNDTRACK_PATH + sound);
+
+        if (!file.exists()) {
+            LOGGER.log(Level.WARNING, "Arquivo não encontrado: {0}", file.getPath());
+            return;
+        }
+
+        try (AudioInputStream audio = AudioSystem.getAudioInputStream(file)) {
+            Clip clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.start();
+            clip.drain();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Erro ao reproduzir o som: {0}", e.getMessage());
+        }
+    }
+
+    private void createDirectory() {
+        File path = new File(SOUNDTRACK_PATH);
+
+        if (path.exists() || path.mkdirs()) {
+            return;
+        }
+
+        LOGGER.log(Level.SEVERE, "Falha ao criar o diretório: {0}", SOUNDTRACK_PATH);
+    }
+}
