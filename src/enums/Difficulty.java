17c057e4cac7b657e170f9452b94b86cd7e91b92
vito-souza <vitorlegalvh@gmail.com>
2024-12-19 19:18:08 -0300

    refactor: construtores e atributos do enum de dificuldade

--- src/enums/Difficulty.java
+++ src/enums/Difficulty.java
@@ -1,5 +1,29 @@
 package enums;
 
 public enum Difficulty {
-    EASY, MEDIUM, HARD
+    EASY(8, 10, 10),
+    MEDIUM(14, 18, 40),
+    HARD(20, 24, 99);
+
+    private final int rows;
+    private final int columns;
+    private final int bombs;
+
+    Difficulty(int rows, int columns, int bombs) {
+        this.rows = rows;
+        this.columns = columns;
+        this.bombs = bombs;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public int getBombs() {
+        return bombs;
+    }
 }
