5112641714a7034f645f0a6b87025b8f6fe469a5
vito-souza <vitorlegalvh@gmail.com>
2024-12-20 21:17:30 -0300

    test: classe soundtrack

--- src/App.java
+++ src/App.java
@@ -3,6 +3,5 @@ import game.audio.Soundtrack;
 public class App {
     public static void main(String[] args) throws Exception {
         Soundtrack.play("portal_radio.wav");
-        // System.out.println("Hello, World!");
     }
 }
