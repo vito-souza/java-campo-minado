a2ccba545ec20b128779c874db031dacde0dd29c
vito-souza <vitorlegalvh@gmail.com>
2024-12-20 21:03:03 -0300

    wip: testando a classe soundtrack

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import game.audio.Soundtrack;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Soundtrack.play("portal_radio.wav");
+        // System.out.println("Hello, World!");
     }
 }
