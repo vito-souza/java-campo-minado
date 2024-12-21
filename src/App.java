250474c811e11305ca06b6d1a703c4555501479f
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-21 13:11:15 -0300

    test: concorrencia entre sfx e bgm

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
+import java.util.Random;
+
 import game.audio.Soundtrack;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Soundtrack.play("portal_radio.wav");
+        Soundtrack.play("music/portal_radio.wav", true);
+
+        Random random = new Random();
+
+        while (true) {
+            if (random.nextInt(5) == 0) {
+                Soundtrack.play("sfx/womp.wav");
+            }
+
+            Thread.sleep(3000);
+        }
     }
 }
